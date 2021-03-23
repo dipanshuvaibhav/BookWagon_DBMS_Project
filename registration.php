5403240f756f3c214359b4d748fb40ecb3cbfe54
Deep <deepp3021@gmail.com>
2021-03-23 23:36:11 +0530

    Fixing registration form

--- registration.php
+++ registration.php
@@ -12,7 +12,7 @@ $result = mysqli_query($con,$s);
 $num = mysqli_num_rows($result);
 
 if($num ==1){
-  echo "<script type='text/javascript'>alert('Username already taken');
+  echo "<script type='text/javascript'>alert('Register with a different email!');
 window.location='login-register.php';
 </script>";
 }else{
