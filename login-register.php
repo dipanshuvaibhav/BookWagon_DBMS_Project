eefcd663add479cd829b40e07296a5981ef6353e
Deep <deepp3021@gmail.com>
2021-03-23 19:06:58 +0530

    created new file registration.php and added necessary things to reg form

--- login-register.php
+++ login-register.php
@@ -478,30 +478,31 @@
 				<div class="row">
 					<div class="col-sm-12 col-md-12 col-xs-12 col-lg-6 mb--30 mb-lg--0">
 						<!-- Login Form s-->
-						<form action="#">
+						<form action="registration.php" method="post">
 							<div class="login-form">
 								<h4 class="login-title">New Customer</h4>
 								<p><span class="font-weight-bold">I am a new customer</span></p>
 								<div class="row">
 									<div class="col-md-12 col-12 mb--15">
 										<label for="email">Full Name</label>
-										<input class="mb-0 form-control" type="text" id="name"
+										<input class="mb-0 form-control" type="text" id="name" name="name"
 											placeholder="Enter your full name" required>
 									</div>
 									<div class="col-12 mb--20">
 										<label for="email">Email</label>
-										<input class="mb-0 form-control" type="email" id="email" placeholder="Enter Your Email Address Here.." required>
+										<input class="mb-0 form-control" type="email" id="email" name="email" placeholder="Enter Your Email Address Here.." required>
 									</div>
 									<div class="col-lg-6 mb--20">
 										<label for="password">Password</label>
-										<input class="mb-0 form-control" type="password" id="password" placeholder="Enter your password" required>
+										<input class="mb-0 form-control" type="password" id="password" name="password" placeholder="Enter your password" required>
 									</div>
 									<div class="col-lg-6 mb--20">
 										<label for="password">Repeat Password</label>
-										<input class="mb-0 form-control" type="password" id="repeat-password" placeholder="Repeat your password" required>
+										<input class="mb-0 form-control" type="password" id="repeat-password" name="repeat-password" placeholder="Repeat your password" required>
 									</div>
 									<div class="col-md-12">
-										<a href="#" class="btn btn-outlined">Register</a>
+										<!-- <a href="#" class="btn btn-outlined">Register</a> -->
+                    <button type="submit" class="btn btn-outlined">Register</button>
 									</div>
 								</div>
 							</div>
