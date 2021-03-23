8e64eaa51ae3cd7be1916d9814c4f94a62565355
Deep <deepp3021@gmail.com>
2021-03-23 22:42:55 +0530

    made a few changes in echo script

--- registration.php
+++ registration.php
@@ -12,12 +12,12 @@ $result = mysqli_query($con,$s);
 $num = mysqli_num_rows($result);
 
 if($num ==1){
-  echo "Username already taken";
+  echo '<script>alert("Username already taken")</script>';
 }else{
   $reg = "insert into heroku_adaaf59afa8e08a.customer_table(cust_name,cust_email,cust_pass) values ('$name','$mail','$pass');";
   mysqli_query($con,$reg);
 
-  echo "Registration Successfull";
+  echo '<script>alert("Registration Successfull!")</script>;
 }
 
  ?>
