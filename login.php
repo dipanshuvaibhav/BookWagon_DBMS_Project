9751b47abeec482fffb15f8886a82378d70e20d6
Dipanshu Vaibhav <53404726+dipanshuvaibhav@users.noreply.github.com>
2021-03-23 20:43:11 +0530

    Update login.php

--- login.php
+++ login.php
@@ -5,11 +5,11 @@ session_start();
 $con = mysqli_connect('us-cdbr-east-03.cleardb.com','b2270625c60d18','74d05350');
 
 mysqli_select_db($con,'heroku_adaaf59afa8e08a');
-// $name =$_POST['name'];
+
 $password = $_POST['login-password'];
 $email = $_POST['email1'];
 
-$s = "select * from customer_table where cust_email = '$email' && cust_pass = '$password';";
+$s = "select * from heroku_adaaf59afa8e08a.customer_table where cust_email = '$email' && cust_pass = '$password'";
 
 $result = mysqli_query($con, $s);
 
