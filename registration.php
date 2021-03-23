b1c6c9eae3c893fb7e51dfed4b06d5d4d9d162a9
Deep <deepp3021@gmail.com>
2021-03-23 22:56:07 +0530

    ...

--- registration.php
+++ registration.php
@@ -12,12 +12,16 @@ $result = mysqli_query($con,$s);
 $num = mysqli_num_rows($result);
 
 if($num ==1){
-  echo '<script>alert("Username already taken")</script>';
+  echo "<script type='text/javascript'>alert('Username already taken');
+window.location='login-register.php';
+</script>";
 }else{
   $reg = "insert into heroku_adaaf59afa8e08a.customer_table(cust_name,cust_email,cust_pass) values ('$name','$mail','$pass');";
   mysqli_query($con,$reg);
 
-  echo '<script>alert("Registration Successfull!")</script>';
+  echo "<script type='text/javascript'>alert('Registration done successfully');
+window.location='login-register.php';
+</script>";
 }
 
  ?>
