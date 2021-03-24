1e32aeca9a34234da5853cf7cdae1c723ee52f70
Deep <deepp3021@gmail.com>
2021-03-24 20:14:29 +0530

    made a few minor changes

--- contact.php
+++ contact.php
@@ -507,7 +507,7 @@
                         <div class="contact-info">
                             <span class="icon"><i class="fa fa-users"></i></span>
                             <div class="content">
-                                <h3 class="title">bookWagon Social</h3>
+                                <h3 class="title">BookWagon Social</h3>
                                 <ul class="social-list list-inline">
                                     <li class="single-social facebook"><a href=""><i class="ion ion-social-facebook"></i></a></li>
                                     <li class="single-social twitter"><a href=""><i class="ion ion-social-twitter"></i></a></li>
@@ -529,85 +529,83 @@
     <!--=================================
     Footer Area
 ===================================== -->
-    <footer class="site-footer">
-        <div class="container">
-            <div class="row justify-content-between  section-padding">
-                <div class=" col-xl-3 col-lg-4 col-sm-6">
-                    <div class="single-footer pb--50">
-                        <div class="brand-footer footer-title">
-                            <img src="#" alt="">
-                        </div>
-                        <div class="footer-contact">
-                            <p><span class="label">Address:</span><span class="text">Christ University, Hosur Main Road, Bangalore</span></p>
-                            <p><span class="label">Phone:</span><span class="text">+18088 CALL BOOKWAGON</span></p>
-                            <p><span class="label">Email:</span><span class="text">suport@bookwagon.com</span></p>
-                        </div>
+<footer class="site-footer">
+    <div class="container">
+        <div class="row justify-content-between  section-padding">
+            <div class=" col-xl-3 col-lg-4 col-sm-6">
+                <div class="single-footer pb--40">
+                    <div class="brand-footer footer-title">
+                        <img src="image/Logo1.png" alt="">
+                    </div>
+                    <div class="footer-contact">
+                        <p><span class="label">Address:</span><span class="text">Sg Palya ,Hosur Road,karnataka,Bangalore
+                                 India</span></p>
+                        <p><span class="label">Phone:</span><span class="text">9887456215</span></p>
+                        <p><span class="label">Email:</span><span class="text">suport@bookswagon.com</span></p>
                     </div>
                 </div>
-                <div class=" col-xl-3 col-lg-2 col-sm-6">
-                    <div class="single-footer pb--50">
-                        <div class="footer-title">
-                            <h3>Information</h3>
-                        </div>
-                        <ul class="footer-list normal-list">
-                            <li><a href="">Prices drop</a></li>
-                            <li><a href="">New products</a></li>
-                            <li><a href="">Best sales</a></li>
-                            <li><a href="">Contact us</a></li>
-                            <li><a href="">Sitemap</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class=" col-xl-3 col-lg-2 col-sm-6">
-                    <div class="single-footer pb--50">
-                        <div class="footer-title">
-                            <h3>Extras</h3>
-                        </div>
-                        <ul class="footer-list normal-list">
-                            <li><a href="">Delivery</a></li>
-                            <li><a href="">About Us</a></li>
-                            <li><a href="">Stores</a></li>
-                            <li><a href="">Contact us</a></li>
-                            <li><a href="">Sitemap</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class=" col-xl-3 col-lg-4 col-sm-6">
+            </div>
+            <div class=" col-xl-3 col-lg-2 col-sm-6">
+                <div class="single-footer pb--40">
                     <div class="footer-title">
-                        <h3>Newsletter Subscribe</h3>
+                        <h3>Information</h3>
                     </div>
-                    <div class="newsletter-form mb--30">
-                        <form action="./php/mail.php">
-                            <input type="email" class="form-control" placeholder="Enter Your Email Address Here...">
-                            <button class="btn btn--primary w-100">Subscribe</button>
-                        </form>
+                    <ul class="footer-list normal-list">
+                        <li><a href="">Contact us</a></li>
+                        <li><a href="">Sitemap</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class=" col-xl-3 col-lg-2 col-sm-6">
+                <div class="single-footer pb--40">
+                    <div class="footer-title">
+                        <h3>Extras</h3>
                     </div>
-                    <div class="social-block">
-                        <h3 class="title">STAY CONNECTED</h3>
-                        <ul class="social-list list-inline">
-                            <li class="single-social facebook"><a href=""><i class="ion ion-social-facebook"></i></a>
-                            </li>
-                            <li class="single-social twitter"><a href=""><i class="ion ion-social-twitter"></i></a></li>
-                            <li class="single-social google"><a href=""><i
-                                        class="ion ion-social-googleplus-outline"></i></a></li>
-                            <li class="single-social youtube"><a href=""><i class="ion ion-social-youtube"></i></a></li>
-                        </ul>
-                    </div>
+                    <ul class="footer-list normal-list">
+                        <!-- <li><a href="">Delivery</a></li> -->
+                        <li><a href="">About Us</a></li>
+                        <!-- <li><a href="">Stores</a></li> -->
+                        <li><a href="">Contact us</a></li>
+                        <li><a href="">Sitemap</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class=" col-xl-3 col-lg-4 col-sm-6">
+                <div class="footer-title">
+                    <h3>Newsletter Subscribe</h3>
+                </div>
+                <div class="newsletter-form mb--30">
+                    <form action="./php/mail.php">
+                        <input type="email" class="form-control" placeholder="Enter Your Email Address Here...">
+                        <button class="btn btn--primary w-100">Subscribe</button>
+                    </form>
+                </div>
+                <div class="social-block">
+                    <h3 class="title">STAY CONNECTED</h3>
+                    <ul class="social-list list-inline">
+                        <li class="single-social facebook"><a href=""><i class="ion ion-social-facebook"></i></a>
+                        </li>
+                        <li class="single-social twitter"><a href=""><i class="ion ion-social-twitter"></i></a></li>
+                        <li class="single-social google"><a href=""><i
+                                    class="ion ion-social-googleplus-outline"></i></a></li>
+                        <li class="single-social youtube"><a href=""><i class="ion ion-social-youtube"></i></a></li>
+                    </ul>
                 </div>
             </div>
         </div>
-        <div class="footer-bottom">
-            <div class="container">
-                <p class="copyright-heading">Why buy a book when you can rent it.</p>
-                <a href="#" class="payment-block">
-                    <img src="image/icon/payment.png" alt="">
-                </a>
-                <p class="copyright-text">Copyright © 2021 <a href="#" class="author">BookWagon</a>. All Right Reserved.
-                    <br>
-                    Design By BookWagon</p>
-            </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <p class="copyright-heading"></p>
+            <a href="#" class="payment-block">
+                <img src="image/icon/payment.png" alt="">
+            </a>
+            <p class="copyright-text">Copyright © 2019 <a href="#" class="author">BookWagon</a>. All Right Reserved.
+                <br>
+                Design By BookWagon</p>
         </div>
-    </footer>
+    </div>
+</footer>
     <script src="js/plugins.js"></script>
     <script src="js/ajax-mail.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB2D8wrWMY3XZnuHO6C31uq90JiuaFzGws"></script>
