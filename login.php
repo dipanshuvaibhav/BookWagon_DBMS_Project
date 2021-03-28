c02b320fc773c823bb0934949cbff6d6524577e3
Deep <deepp3021@gmail.com>
2021-03-28 20:30:10 +0530

    ..

--- login.php
+++ login.php
@@ -1,14 +1,12 @@
 <?php
-$con = mysqli_connect('us-cdbr-east-03.cleardb.com','b2270625c60d18','74d05350');
-
-mysqli_select_db($con,'heroku_adaaf59afa8e08a');
+include "dbconnect";
 // $name =$_POST['name'];
 $password = $_POST['password'];
 $email = $_POST['email1'];
 
 $s = "select * from heroku_adaaf59afa8e08a.customer_table where cust_email = '$email' && cust_pass = '$password';";
 
-$result = mysqli_query($con, $s);
+$result = mysqli_query($conn, $s);
 
 $num = mysqli_num_rows($result);
 
