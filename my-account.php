21e56a351cb615cd897b8aeb7d99c8c0b10851bc
Deep <deepp3021@gmail.com>
2021-03-29 14:36:02 +0530

    added a css class to php <p> tag

--- my-account.php
+++ my-account.php
@@ -175,7 +175,7 @@ session_start();
 									<div class="login-block">
                     <?php
                       if(isset($_SESSION['id'])){
-                      echo  "<p>Welcome,".$_SESSION['name']."</p>";
+                      echo  "<p class='font-weight-bold'>Welcome,".$_SESSION['name']."</p>";
                       } else {
                       echo '"<a href="login-register.php" class="font-weight-bold">Login</a> <br>
                       <span>or</span><a href="login-register.php">Register</a>"';
