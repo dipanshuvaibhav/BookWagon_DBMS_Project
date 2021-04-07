9d80fa9c0274e49f6afd485445233ad1e5e11cc7
Deep <deepp3021@gmail.com>
2021-04-07 19:23:21 +0530

    fixed errors in action.php

--- action.php
+++ action.php
@@ -9,15 +9,15 @@
    $p_code = $_POST['p_code'];
    $p_qty = 1;
 
-   $stmt = $comm->prepare("SELECT p_code FROM heroku_adaaf59afa8e08a.cart WHERE p_code=?;");
+   $stmt = $comm->prepare("SELECT p_code FROM heroku_adaaf59afa8e08a.cart WHERE p_code=?");
    $stmt->bind_param("s",$p_code);
    $stmt->execute();
    $res = $stmt->get_result();
-   $r = $res-fetch_assoc();
+   $r = $res->fetch_assoc();
    $code = $r['p_code'];
 
    if(!$code){
-     $query = $conn->prepare("INSERT INTO heroku_adaaf59afa8e08a.cart (p_name,p_price,p_image,c_qty,tot_price,p_code) VALUES (?,?,?,?,?,?); ");
+     $query = $conn->prepare("INSERT INTO heroku_adaaf59afa8e08a.cart (p_name,p_price,p_image,c_qty,tot_price,p_code) VALUES (?,?,?,?,?,?)");
      $query->bind_param("sssiss",$p_name,$p_price,$p_image,$p_qty,$p_price,$p_code);
      $query->execute();
 
