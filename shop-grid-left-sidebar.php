27cee33dbe57038779a4e5dd359af6c48a58a041
Deep <deepp3021@gmail.com>
2021-04-08 16:45:48 +0530

    ...

--- shop-grid-left-sidebar.php
+++ shop-grid-left-sidebar.php
@@ -177,50 +177,7 @@ session_start();
 													<!-- Shop -->
 													<li class="menu-item  mega-menu">
 																						<a href="shop-grid-left-sidebar.php">shop </a>
-																						<!-- <ul class="sub-menu four-column">
-																								<li class="cus-col-25">
-																										<h3 class="menu-title"><a href="javascript:void(0)">Shop</a></h3>
-																										<ul class="mega-single-block">
-																												<li><a href="shop-grid.php">Fullwidth</a></li>
-																												<li><a href="shop-grid-left-sidebar.php">Shop</a></li>
-																												<li><a href="shop-grid-right-sidebar.php">Right Sidebar</a></li>
-																										</ul> -->
 																								</li>
-																								<!-- <li class="cus-col-25">
-																										<h3 class="menu-title"> <a href="javascript:void(0)">Shop List</a></h3>
-																										<ul class="mega-single-block">
-																												<li><a href="shop-list.php">Fullwidth</a></li>
-																												<li><a href="shop-list-left-sidebar.php">List</a></li>
-																											<li><a href="shop-list-right-sidebar.php">Right Sidebar</a></li>
-																										</ul>
-																								</li>-->
-																								<!-- <li class="cus-col-25">
-																										<h3 class="menu-title"> <a href="javascript:void(0)">Product Details
-																														</a></h3>
-																										<ul class="mega-single-block">
-																												<li><a href="product-details.php">Product Details Page</a></li>
-																												<li><a href="product-details-affiliate.php">Product Details
-																																Affiliate</a></li>
-																												<li><a href="product-details-group.php">Product Details Group</a>
-																												</li>
-																												<li><a href="product-details-variable.php">Product Details
-																																Variables</a></li>
-																										</ul>
-																								</li> -->
-																								<!-- <li class="cus-col-25">
-																										<h3 class="menu-title"><a href="javascript:void(0)">Product Details
-																														2</a></h3>
-																										<ul class="mega-single-block">
-																												<li><a href="product-details-left-thumbnail.php">Thumbnail</a>
-																												</li> -->
-																												<!-- <li><a href="product-details-right-thumbnail.php">Right Thumbnail</a></li> -->
-																												<!-- <li><a href="product-details-left-gallery.php">Gallery</a>
-																												</li> -->
-																												<!-- <li><a href="product-details-right-gallery.php">Right Gallery</a>
-																												</li> -->
-																										<!-- </ul>
-																								</li> -->
-
 																				</li>
 													<!-- Pages -->
 													<li class="menu-item has-children">
@@ -242,35 +199,7 @@ session_start();
 													<!-- Blog -->
 													<li class="menu-item mega-menu">
 															<a href="blog-list.php">Blog </a>
-															<!-- <ul class="sub-menu three-column">
-																	<li class="cus-col-33">
-																			<h3 class="menu-title"><a href="javascript:void(0)">Blog Grid</a></h3>
-																			<ul class="mega-single-block">
-																					<li><a href="blog.php">Full Widh (Default)</a></li>
-																					<li><a href="blog-left-sidebar.php">left Sidebar</a></li>
-																					<li><a href="blog-right-sidebar.php">Right Sidebar</a></li>
-																			</ul>
-																	</li>
-																	<li class="cus-col-33">
-																			<h3 class="menu-title"><a href="javascript:void(0)">Blog List </a></h3>
-																			<ul class="mega-single-block">
-																					<li><a href="blog-list.php">Full Widh (Default)</a></li>
-																					<li><a href="blog-list-left-sidebar.php">left Sidebar</a></li>
-																					<li><a href="blog-list-right-sidebar.php">Right Sidebar</a></li>
-																			</ul>
-																	</li>
-																	<li class="cus-col-33">
-																			<h3 class="menu-title"><a href="javascript:void(0)">Blog Details</a>
-																			</h3>
-																			<ul class="mega-single-block">
-																					<li><a href="blog-details.php">Image Format (Default)</a></li>
-																					<li><a href="blog-details-gallery.php">Gallery</a></li>
-																					<li><a href="blog-details-audio.php">Audio Format</a></li>
-																					<li><a href="blog-details-video.php">Video Format</a></li>
-																					<li><a href="blog-details-left-sidebar.php">Blog</a></li>
-																			</ul>
-																	</li>
-															</ul> -->
+
 													</li>
 													<li class="menu-item">
 															<a href="contact.php">Contact</a>
@@ -389,220 +318,15 @@ session_start();
 											</div>
 										</div>
 									</div>
-									<!-- <div class="product-list-content">
-										<div class="card-image">
-											<img src="image/products/product-3.jpg" alt="">
-										</div>
-										<div class="product-card--body">
-											<div class="product-header">
-												<a href="" class="author">
-													Gpple
-												</a>
-												<h3><a href="product-details.php" tabindex="0">Qpple cPad with Retina
-														Display MD510LL/A</a></h3>
-											</div>
-											<article>
-												<h2 class="sr-only">Card List Article</h2>
-												<p>More room to move. With 80GB or 160GB of storage and up to 40 hours
-													of battery life, the new iPod classic lets you enjoy
-													up to 40,000 songs or..</p>
-											</article>
-											<div class="price-block">
-												<span class="price">₹51.20</span>
-												<del class="price-old">₹51.20</del>
-												<span class="price-discount">20%</span>
-											</div>
-											<div class="rating-block">
-												<span class="fas fa-star star_on"></span>
-												<span class="fas fa-star star_on"></span>
-												<span class="fas fa-star star_on"></span>
-												<span class="fas fa-star star_on"></span>
-												<span class="fas fa-star "></span>
-											</div>
-											<div class="btn-block">
-												<a href="" class="btn btn-outlined">Add To Cart</a>
-												<a href="" class="card-link"><i class="fas fa-heart"></i> Add To
-													Wishlist</a>
-												<a href="" class="card-link"><i class="fas fa-random"></i> Add To
-													Cart</a>
-											</div>
-										</div>
-									</div> -->
+
 								</div>
 							</div>
 						<?php endwhile;?>
 						</div>
 						<!-- Pagination Block -->
-						<!-- <div class="row pt--30">
-							<div class="col-md-12">
-								<div class="pagination-block">
-									<ul class="pagination-btns flex-center">
-										<li><a href="" class="single-btn prev-btn ">|<i
-													class="zmdi zmdi-chevron-left"></i> </a></li>
-										<li><a href="" class="single-btn prev-btn "><i
-													class="zmdi zmdi-chevron-left"></i> </a></li>
-										<li class="active"><a href="" class="single-btn">1</a></li>
-										<li><a href="" class="single-btn">2</a></li>
-										<li><a href="" class="single-btn">3</a></li>
-										<li><a href="" class="single-btn">4</a></li>
-										<li><a href="" class="single-btn next-btn"><i
-													class="zmdi zmdi-chevron-right"></i></a></li>
-										<li><a href="" class="single-btn next-btn"><i
-													class="zmdi zmdi-chevron-right"></i>|</a></li>
-									</ul>
-								</div>
-							</div>
-						</div> -->
-						<!-- Modal -->
-						<div class="modal fade modal-quick-view" id="quickModal" tabindex="-1" role="dialog"
-							aria-labelledby="quickModal" aria-hidden="true">
-							<div class="modal-dialog" role="document">
-								<div class="modal-content">
-									<button type="button" class="close modal-close-btn ml-auto" data-dismiss="modal"
-										aria-label="Close">
-										<span aria-hidden="true">&times;</span>
-									</button>
-									<div class="product-details-modal">
-										<div class="row">
-											<div class="col-lg-5">
-												<!-- Product Details Slider Big Image-->
-												<div class="product-details-slider sb-slick-slider arrow-type-two"
-													data-slick-setting='{
-              "slidesToShow": 1,
-              "arrows": false,
-              "fade": true,
-              "draggable": false,
-              "swipe": false,
-              "asNavFor": ".product-slider-nav"
-              }'>
-													<div class="single-slide">
-														<img src="image/products/product-details-1.jpg" alt="">
-													</div>
-													<div class="single-slide">
-														<img src="image/products/product-details-2.jpg" alt="">
-													</div>
-													<div class="single-slide">
-														<img src="image/products/product-details-3.jpg" alt="">
-													</div>
-													<div class="single-slide">
-														<img src="image/products/product-details-4.jpg" alt="">
-													</div>
-													<div class="single-slide">
-														<img src="image/products/product-details-5.jpg" alt="">
-													</div>
-												</div>
-												<!-- Product Details Slider Nav -->
-												<div class="mt--30 product-slider-nav sb-slick-slider arrow-type-two"
-													data-slick-setting='{
-            "infinite":true,
-              "autoplay": true,
-              "autoplaySpeed": 8000,
-              "slidesToShow": 4,
-              "arrows": true,
-              "prevArrow":{"buttonClass": "slick-prev","iconClass":"fa fa-chevron-left"},
-              "nextArrow":{"buttonClass": "slick-next","iconClass":"fa fa-chevron-right"},
-              "asNavFor": ".product-details-slider",
-              "focusOnSelect": true
-              }'>
-													<div class="single-slide">
-														<img src="image/products/product-details-1.jpg" alt="">
-													</div>
-													<div class="single-slide">
-														<img src="image/products/product-details-2.jpg" alt="">
-													</div>
-													<div class="single-slide">
-														<img src="image/products/product-details-3.jpg" alt="">
-													</div>
-													<div class="single-slide">
-														<img src="image/products/product-details-4.jpg" alt="">
-													</div>
-													<div class="single-slide">
-														<img src="image/products/product-details-5.jpg" alt="">
-													</div>
-												</div>
-											</div>
-											<div class="col-lg-7 mt--30 mt-lg--30">
-												<div class="product-details-info pl-lg--30 ">
-													<p class="tag-block">Tags: <a href="#">Movado</a>, <a
-															href="#">Omega</a></p>
-													<h3 class="product-title">Beats EP Wired On-Ear Headphone-Black</h3>
-													<ul class="list-unstyled">
-														<li>Ex Tax: <span class="list-value"> ₹60.24</span></li>
-														<li>Brands: <a href="#" class="list-value font-weight-bold">
-																Canon</a></li>
-														<li>Product Code: <span class="list-value"> model1</span></li>
-														<li>Reward Points: <span class="list-value"> 200</span></li>
-														<li>Availability: <span class="list-value"> In Stock</span></li>
-													</ul>
-													<div class="price-block">
-														<span class="price-new">₹73.79</span>
-														<del class="price-old">₹91.86</del>
-													</div>
-													<div class="rating-widget">
-														<div class="rating-block">
-															<span class="fas fa-star star_on"></span>
-															<span class="fas fa-star star_on"></span>
-															<span class="fas fa-star star_on"></span>
-															<span class="fas fa-star star_on"></span>
-															<span class="fas fa-star "></span>
-														</div>
-														<div class="review-widget">
-															<a href="">(1 Reviews)</a> <span>|</span>
-															<a href="">Write a review</a>
-														</div>
-													</div>
-													<article class="product-details-article">
-														<h4 class="sr-only">Product Summery</h4>
-														<p>Long printed dress with thin adjustable straps. V-neckline
-															and wiring under the Dust with ruffles at the bottom
-															of the
-															dress.</p>
-													</article>
-													<div class="add-to-cart-row">
-														<div class="count-input-block">
-															<span class="widget-label">Qty</span>
-															<input type="number" class="form-control text-center"
-																value="1">
-														</div>
-														<div class="add-cart-btn">
-															<a href="" class="btn btn-outlined--primary"><span
-																	class="plus-icon">+</span>Add to Cart</a>
-														</div>
-													</div>
-													<div class="compare-wishlist-row">
-														<a href="" class="add-link"><i class="fas fa-heart"></i>Add to
-															Wish List</a>
-														<a href="" class="add-link"><i class="fas fa-random"></i>Add to
-															Compare</a>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-									<div class="modal-footer">
-										<div class="widget-social-share">
-											<span class="widget-label">Share:</span>
-											<div class="modal-social-share">
-												<a href="#" class="single-icon"><i class="fab fa-facebook-f"></i></a>
-												<a href="#" class="single-icon"><i class="fab fa-twitter"></i></a>
-												<a href="#" class="single-icon"><i class="fab fa-youtube"></i></a>
-												<a href="#" class="single-icon"><i class="fab fa-google-plus-g"></i></a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3  mt--40 mt-lg--0">
-						<div class="inner-page-sidebar">
 
-						</div>
-					</div>
-				</div>
-			</div>
-		</main>
-	</div>
+						<!-- Modal -->
+						
 	<!--=================================
   Brands Slider
 ===================================== -->
