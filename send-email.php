61d1ef47ee1e33cbe7b9f1a0c7d75c783815441d
Dipanshu Vaibhav <53404726+dipanshuvaibhav@users.noreply.github.com>
2021-04-08 14:47:43 +0530

    send email php code

---
+++ send-email.php
@@ -0,0 +1,33 @@
+<?php 
+ 
+ $user = "b2270625c60d18";  
+ $password = "74d05350";  
+ $host = "us-cdbr-east-03.cleardb.com";  
+ $dbase = "heroku_adaaf59afa8e08a";  
+ $table = "email_list";  
+   
+
+$from= 'dipanshu.vaibhav@science.christuniversity.in';//specify here the address that you want email to be sent from 
+ 
+$subject= $_POST['subject']; 
+$body= $_POST['body']; 
+ 
+// Connection to DBase  
+$dbc= mysqli_connect($host,$user,$password, $dbase)  
+or die("Unable to select database"); 
+ 
+$query= "SELECT * FROM heroku_adaaf59afa8e08a.email_list"; 
+$result= mysqli_query ($dbc, $query)  
+or die ('Error querying database.'); 
+ 
+while ($row = mysqli_fetch_array($result)) { 
+$email= $row['email']; 
+ 
+$msg= "Dear BookWagoner,\n$body"; 
+mail($email, $subject, $msg, 'From:' . $from); 
+echo 'Email sent to: ' . $email. '<br>'; 
+} 
+ 
+mysqli_close($dbc); 
+?> 
+ 
---
+++ send-newsletter.php
@@ -0,0 +1,3 @@
+<form action="send-email.php" method="post"> 
+<label>Subject of email:</label><br><input type="text" name="subject" id="subject"/><br> 
+<label>Body of email:</label><br><textarea cols="35" rows="10" name="Comments">
