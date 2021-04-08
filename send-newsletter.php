d437a4dcebf41645f735f1ac218e67dbcf660d7f
Dipanshu Vaibhav <53404726+dipanshuvaibhav@users.noreply.github.com>
2021-04-08 15:02:32 +0530

    ..

--- send-newsletter.php
+++ send-newsletter.php
@@ -1,3 +1,5 @@
 <form action="send-email.php" method="post"> 
 <label>Subject of email:</label><br><input type="text" name="subject" id="subject"/><br> 
 <label>Body of email:</label><br><textarea cols="35" rows="10" name="Comments">
+<input type="submit" name=submit value="Submit"/> 
+</form> 
