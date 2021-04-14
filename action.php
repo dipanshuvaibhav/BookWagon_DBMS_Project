6f09b76384109a7fc77266a7bed4c8b896612125
Deep <deepp3021@gmail.com>
2021-04-14 20:08:59 +0530

    ..//..//.

--- action.php
+++ action.php
@@ -81,7 +81,7 @@ session_start();
 
    $data = '';
    $stmt = $conn->prepare("INSERT INTO heroku_adaaf59afa8e08a.order(customer_id,o_name,o_email,o_phone,o_address,o_pmode,o_products,o_amt_paid) VALUES (?,?,?,?,?,?,?,?)");
-   $stmt->bind_param(isssssss,$cust_id,$name,$email,$phone,$address,$pmode,$products,$grand_total);
+   $stmt->bind_param("isssssss",$cust_id,$name,$email,$phone,$address,$pmode,$products,$grand_total);
    $stmt->execute();
    $data .= '"<div class="text-center">
               <h1 class="display-4 mt-2 ">Thank you!</h1>
