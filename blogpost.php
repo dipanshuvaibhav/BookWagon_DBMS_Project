caaf469eb7860e46239813c6241823cc9abdeb06
Deep <deepp3021@gmail.com>
2021-04-19 23:42:33 +0530

    trying firebase for the image upload part

--- blogpost.php
+++ blogpost.php
@@ -299,6 +299,39 @@ session_start();
 				</div>
 		</div>
 </footer>
+<script src="https://www.gstatic.com/firebasejs/8.3.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBoOYTPzpTLpB2nnhPOR7OO91fjtb7LYYM",
+    authDomain: "bookwagondb-cdc14.firebaseapp.com",
+    projectId: "bookwagondb-cdc14",
+    storageBucket: "bookwagondb-cdc14.appspot.com",
+    messagingSenderId: "621596754563",
+    appId: "1:621596754563:web:ed995a212d23aeb00e51b6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //Getting File
+  var filebutton = document.getElementById('image');
+
+  // listen for file selection
+  filebutton.addEventListener('change', function(e){
+    // Get File
+    var file = e.target.files[0];
+   // Create a storage ref
+  var storageref =  firebase.storage().ref('/image/blog_images/'+ file.name);
+  // Upload file
+  storageref.put(file);
+
+  });
+</script>
+
 	<!-- Use Minified Plugins Version For Fast Page Load -->
 	<script src="js/plugins.js"></script>
 	<script src="js/ajax-mail.js"></script>
