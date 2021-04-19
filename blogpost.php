56b6b036a0b66cb5cbcb268e2adf11dadc2a808a
Deep <deepp3021@gmail.com>
2021-04-19 23:58:10 +0530

    ./././.

--- blogpost.php
+++ blogpost.php
@@ -299,23 +299,26 @@ session_start();
 				</div>
 		</div>
 </footer>
-<script src="https://www.gstatic.com/firebasejs/8.3.0/firebase-app.js"></script>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js"></script>
 
-<!-- TODO: Add SDKs for Firebase products that you want to use
+
+<!-- : Add SDKs for Firebase products that you want to use
      https://firebase.google.com/docs/web/setup#available-libraries -->
 
 <script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyBoOYTPzpTLpB2nnhPOR7OO91fjtb7LYYM",
-    authDomain: "bookwagondb-cdc14.firebaseapp.com",
-    projectId: "bookwagondb-cdc14",
-    storageBucket: "bookwagondb-cdc14.appspot.com",
-    messagingSenderId: "621596754563",
-    appId: "1:621596754563:web:ed995a212d23aeb00e51b6"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBoOYTPzpTLpB2nnhPOR7OO91fjtb7LYYM",
+  authDomain: "bookwagondb-cdc14.firebaseapp.com",
+  projectId: "bookwagondb-cdc14",
+  storageBucket: "bookwagondb-cdc14.appspot.com",
+  messagingSenderId: "621596754563",
+  appId: "1:621596754563:web:ed995a212d23aeb00e51b6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
   //Getting File
   var filebutton = document.getElementById('image');
@@ -325,7 +328,7 @@ session_start();
     // Get File
     var file = e.target.files[0];
    // Create a storage ref
-  var storageref =  firebase.storage().ref('/image/blog_images/'+ file.name);
+  var storageref =  firebase.storage().ref('image/blog_images/'+ file.name);
   // Upload file
   storageref.put(file);
 
