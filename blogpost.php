ec69b7499b51c17d50fa400a8f80be1e80875d80
Deep <deepp3021@gmail.com>
2021-04-20 23:04:09 +0530

    ./././.

--- blogpost.php
+++ blogpost.php
@@ -332,7 +332,7 @@ session_start();
   var storageRef =  firebase.storage().ref('image/blog_images/'+ file.name).put(file);
   // Upload file
    // storageRef.put(file);
-  var urlImage = storageRef.getDownloadURL();
+  var urlImage = storageRef.getDownloadUrl();
   // setting cookie
  document.cookie = "imageURL=" + String(urlImage) + ";";
   // Cookies.set('imageurl',urlImage);
