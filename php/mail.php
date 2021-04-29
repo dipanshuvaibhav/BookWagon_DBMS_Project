0210e7e04d2486a88b0795d7ffd8511a57486f5f
Deep <deepp3021@gmail.com>
2021-04-29 11:04:17 +0530

    trying out mail part

--- php/mail.php
+++ php/mail.php
@@ -24,16 +24,16 @@ header("Access-Control-Allow-Origin: *");
         $subject = "Test Email for Template Demo - Mail From $name";
 
         // Build the email content.
-        $email_content = "Name: $name\n";
+        $email_content = "From: $name\n";
         $email_content .= "Email: $email\n\n";
-        $email_content .= "Email: $phone\n\n";
+        // $email_content .= "Email: $phone\n\n";
         $email_content .= "Message:\n$message\n";
 
         // Build the email headers.
         $email_headers = "From: $name <$email>";
 
         // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
+        if (mail($recipient, $subject, $email_content)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
