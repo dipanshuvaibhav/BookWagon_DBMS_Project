cb15ab05323296201f3f9fc834c648ed7ee9b4a1
sayarip <77108289+sayarip@users.noreply.github.com>
2021-04-30 05:20:48 +0530

    Update searchpage.php

--- searchpage.php
+++ searchpage.php
@@ -3,7 +3,7 @@
     $str= $_POST['search'];
 	$stmt = $conn->prepare('SELECT * FROM heroku_adaaf59afa8e08a.product where p_name like '%$str%';');
 	$stmt->execute();
-    $result = $stmt->get_result();
+    //$result = $stmt->get_result();
     echo "<script type='text/javascript'>alert('successfully!');
     window.location='blogpost.php';
     </script>";
