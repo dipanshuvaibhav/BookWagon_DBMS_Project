089c9128ae5c5407e0eb9e93cf3b3b49a8556826
sayarip <77108289+sayarip@users.noreply.github.com>
2021-04-30 05:09:46 +0530

    Update searchpage.php

--- searchpage.php
+++ searchpage.php
@@ -179,10 +179,10 @@ session_start();
 						<div class="shop-product-wrap grid with-pagination row space-db--30 shop-border">
 							<?php
 								include 'config.php';
-								
-                                				$str= mysqli_real_escape_string($con,$_POST['search']);
-								$sql = "SELECT * FROM heroku_adaaf59afa8e08a.product where p_name like '%$str%'";
-								$result = mysqli_query($con,$sql);
+                                				$str= $_POST['search'];
+								$stmt = $conn->prepare('SELECT * FROM heroku_adaaf59afa8e08a.product where p_name like '%$str%';');
+								$stmt->execute();
+								$result = $stmt->get_result();
 								
                               					
                                 
