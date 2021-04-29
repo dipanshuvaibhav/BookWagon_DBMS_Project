817606f2422a010396a5f932547afa258fb7da3c
Deep <deepp3021@gmail.com>
2021-04-29 17:25:57 +0530

    Revert "Update shop-grid-left-sidebar.php"
    This reverts commit 3bab90cf80c45e015378cfada457b9b94230e301.

--- shop-grid-left-sidebar.php
+++ shop-grid-left-sidebar.php
@@ -153,7 +153,51 @@ session_start();
 						</div>
 				</div>
 		</div>
-		
+		<section class="hero-area hero-slider-4 ">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9 offset-lg-3">
+                        <div class="sb-slick-slider" data-slick-setting='{
+                                                                    "autoplay": true,
+                                                                    "autoplaySpeed": 8000,
+                                                                    "slidesToShow": 1,
+                                                                    "dots":true
+                                                                    }'>
+                            <div class="single-slide bg-image bg-overlay--white"
+                                data-bg="image/bg-images/home-4-slider-1.jpg">
+                                <div class="home-content text-left pl--30">
+                                    <div class="row">
+                                        <div class="col-lg-5">
+                                            <span class="title-small">Cover</span>
+                                            <h1>Mockup.</h1>
+                                            <p>Cover up front of book and
+                                                <br>leave summary</p>
+                                            <a href="shop-grid-left-sidebar.php" class="btn btn-outlined--pink">
+                                                Shop Now
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="single-slide bg-image bg-overlay--dark"
+                                data-bg="image/bg-images/home-4-slider-2.jpg">
+                                <div class="home-content text-center">
+                                    <div class="row justify-content-end">
+                                        <div class="col-lg-8">
+                                            <h1 class="v2">Cover</h1>
+                                            <h2></h2>
+                                            <a href="shop-grid-left-sidebar.php" class="btn btn--yellow">
+                                                Shop Now
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 		<main class="inner-page-sec-padding-bottom">
 			<!-- adding success bar if item added to cart -->
 			<div id="message">
