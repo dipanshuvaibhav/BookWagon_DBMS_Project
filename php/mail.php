9c663eae1e3614d97c750b56fe70d8ae9c885757
Deep <deepp3021@gmail.com>
2021-04-29 10:35:27 +0530

    trying ou mail page in contact us

--- php/mail.php
+++ php/mail.php
@@ -8,7 +8,7 @@ header("Access-Control-Allow-Origin: *");
         $email = filter_var(trim($_POST["con_email"]), FILTER_SANITIZE_EMAIL);
         $phone = strip_tags(trim($_POST["con_phone"]));
         $message = trim($_POST["con_message"]);
- 
+
         // Check that data was sent to the mailer.
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
@@ -16,22 +16,22 @@ header("Access-Control-Allow-Origin: *");
             echo "Please complete the form and try again.";
             exit;
         }
- 
+
         // Set the recipient email address.
-        $recipient = "test@hasthemes.com";
- 
+        $recipient = "deepp3021@gmail.com";
+
         // Set the email subject.
         $subject = "Test Email for Template Demo - Mail From $name";
- 
+
         // Build the email content.
         $email_content = "Name: $name\n";
         $email_content .= "Email: $email\n\n";
         $email_content .= "Email: $phone\n\n";
         $email_content .= "Message:\n$message\n";
- 
+
         // Build the email headers.
         $email_headers = "From: $name <$email>";
- 
+
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
@@ -42,11 +42,11 @@ header("Access-Control-Allow-Origin: *");
             http_response_code(500);
             echo "Oops! Something went wrong and we couldn't send your message.";
         }
- 
+
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
         echo "There was a problem with your submission, please try again.";
     }
- 
+
 ?>
