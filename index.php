e47f27c0340042876778d71d37701b4067ed1214
sayarip <77108289+sayarip@users.noreply.github.com>
2021-04-30 17:38:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1231,7 +1231,7 @@ session_start();
                         </div>
                     </div>
                 </div>
-                <div class=" col-xl-3 col-lg-2 col-sm-6">
+               <!-- <div class=" col-xl-3 col-lg-2 col-sm-6">
                     <div class="single-footer pb--40">
                         <div class="footer-title">
                             <h3>Information</h3>
@@ -1248,14 +1248,14 @@ session_start();
                             <h3>Extras</h3>
                         </div>
                         <ul class="footer-list normal-list">
-                            <!-- <li><a href="">Delivery</a></li> -->
+                           
                             <li><a href="">About Us</a></li>
-                            <!-- <li><a href="">Stores</a></li> -->
+                            
                             <li><a href="contact.php">Contact us</a></li>
                             <li><a href="">Sitemap</a></li>
                         </ul>
                     </div>
-                </div>
+                </div>-->
                 <div class=" col-xl-3 col-lg-4 col-sm-6">
                     <div class="footer-title">
                         <h3>Newsletter Subscribe</h3>
