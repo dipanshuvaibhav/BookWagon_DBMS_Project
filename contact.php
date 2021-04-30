0ab395283b4c451fc9e157daa527dd738b5e7bc7
sayarip <77108289+sayarip@users.noreply.github.com>
2021-04-30 18:10:48 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -307,30 +307,7 @@ session_start();
                         <p><span class="label">Email:</span><span class="text">suport@bookswagon.com</span></p>
                     </div>
                 </div>
-            </div>
-            <div class=" col-xl-3 col-lg-2 col-sm-6">
-                <div class="single-footer pb--40">
-                    <div class="footer-title">
-                        <h3>Information</h3>
-                    </div>
-                    <ul class="footer-list normal-list">
-                        <li><a href="">Contact us</a></li>
-                        <li><a href="">Sitemap</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class=" col-xl-3 col-lg-2 col-sm-6">
-                <div class="single-footer pb--40">
-                    <div class="footer-title">
-                        <h3>Extras</h3>
-                    </div>
-                    <ul class="footer-list normal-list">
-                        <li><a href="">About Us</a></li>
-                        <li><a href="">Contact us</a></li>
-                        <li><a href="">Sitemap</a></li>
-                    </ul>
-                </div>
-            </div>
+		</div>
             <div class=" col-xl-3 col-lg-4 col-sm-6">
                 <div class="footer-title">
                     <h3>Newsletter Subscribe</h3>
