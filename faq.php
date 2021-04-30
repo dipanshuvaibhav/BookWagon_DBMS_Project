25b55056f6e007436d0478442be362628508304b
sayarip <77108289+sayarip@users.noreply.github.com>
2021-04-30 18:11:55 +0530

    Update faq.php

--- faq.php
+++ faq.php
@@ -293,29 +293,6 @@ session_start();
 										</div>
 								</div>
 						</div>
-						<div class=" col-xl-3 col-lg-2 col-sm-6">
-								<div class="single-footer pb--40">
-										<div class="footer-title">
-												<h3>Information</h3>
-										</div>
-										<ul class="footer-list normal-list">
-												<li><a href="contact.php">Contact us</a></li>
-												<li><a href="">Sitemap</a></li>
-										</ul>
-								</div>
-						</div>
-						<div class=" col-xl-3 col-lg-2 col-sm-6">
-								<div class="single-footer pb--40">
-										<div class="footer-title">
-												<h3>Extras</h3>
-										</div>
-										<ul class="footer-list normal-list">
-												<li><a href="faq.php">About Us</a></li>
-												<li><a href="contact.php">Contact us</a></li>
-												<li><a href="">Sitemap</a></li>
-										</ul>
-								</div>
-						</div>
 						<div class=" col-xl-3 col-lg-4 col-sm-6">
 								<div class="footer-title">
 										<h3>Newsletter Subscribe</h3>
