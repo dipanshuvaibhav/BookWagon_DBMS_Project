80332f4efdaa143abb41f2ecb45491824c8a9218
Deep <deepp3021@gmail.com>
2021-05-03 10:45:42 +0530

    deleting more unwanted stuff

--- paymentGateway.php
+++
@@ -1,9 +0,0 @@
-<div class="razorpay-embed-btn" data-url="https://pages.razorpay.com/pl_GsZPA0BP48Co2D/view" data-text="Pay Now" data-color="#528FF0" data-size="large">
-  <script>
-    (function(){
-      var d=document; var x=!d.getElementById('razorpay-embed-btn-js')
-      if(x){ var s=d.createElement('script'); s.defer=!0;s.id='razorpay-embed-btn-js';
-      s.src='https://cdn.razorpay.com/static/embed_btn/bundle.js';d.body.appendChild(s);} else{var rzp=window['_rzp_'];
-      rzp && rzp.init && rzp.init()}})();
-  </script>
-</div>
