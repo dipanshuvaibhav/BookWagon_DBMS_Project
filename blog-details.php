3919f686abe861da443836b4b74dfe869734f4e2
sayarip <77108289+sayarip@users.noreply.github.com>
2021-05-03 09:27:49 +0530

    Update blog-details.php

--- blog-details.php
+++ blog-details.php
@@ -210,137 +210,14 @@ session_start();
                     </div>
 										<?php endwhile; ?>
                 </div>
-                <!-- <div class="share-block mb--50">
-                    <h3>Share Now</h3>
-                    <div class="social-links  justify-content-center  mt--10">
-                        <a href="#" class="single-social social-rounded"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#" class="single-social social-rounded"><i class="fab fa-twitter"></i></a>
-                        <a href="#" class="single-social social-rounded"><i class="fab fa-pinterest-p"></i></a>
-                        <a href="#" class="single-social social-rounded"><i class="fab fa-google-plus-g"></i></a>
-                        <a href="#" class="single-social social-rounded"><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                </div> -->
-                <!-- <div class="comment-block-wrapper mb--50">
-                    <h3>3 Comments</h3>
-                    <div class="single-comment">
-                        <div class="comment-avatar">
-                            <img src="image/icon/author-logo.png" alt="">
-                        </div>
-                        <div class="comment-text">
-                            <h5 class="author"> <a href="#"> Author</a></h5>
-                            <span class="time">October 8, 2014 at 12:38 pm</span>
-                            <p>Ame No Parade</p>
-                        </div>
-                        <a href="#" class="btn btn-outlined--primary btn-rounded reply-btn">Reply</a>
-                    </div>
-                    <div class="single-comment">
-                        <div class="comment-avatar">
-                            <img src="image/icon/author-logo.png" alt="">
-                        </div>
-                        <div class="comment-text">
-                            <h5 class="author"> <a href="#">Jack</a></h5>
-                            <span class="time">January 19, 2018 at 3:00 am</span>
-                            <p>just a nice post</p>
-                        </div>
-                        <a href="#" class="btn btn-outlined--primary btn-rounded reply-btn">Reply</a>
-                    </div>
-                    <div class="single-comment">
-                        <div class="comment-avatar">
-                            <img src="image/icon/author-logo.png" alt="">
-                        </div>
-                        <div class="comment-text">
-                            <h5 class="author"> <a href="#">Dexter</a></h5>
-                            <span class="time">august 31, 2021 at 3:30 am</span>
-                            <p>Awesome Post </p>
-                        </div>
-                        <a href="#" class="btn btn-outlined--primary btn-rounded reply-btn">Reply</a>
-                    </div>
-                </div> -->
-                <!-- <div class="replay-form-wrapper">
-                    <h3 class="mt-0">LEAVE A REPLY</h3>
-                    <p>Your email address will not be published. Required fields are marked *</p>
-                    <form action="./" class="blog-form">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label for="message">Comment</label>
-                                    <textarea name="message" id="message" cols="30" rows="10"
-                                        class="form-control"></textarea>
-                                </div>
-                            </div>
-                            <div class="col-lg-4">
-                                <div class="form-group">
-                                    <label for="name">Name *</label>
-                                    <input type="text" id="name" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-lg-4">
-                                <div class="form-group">
-                                    <label for="email">Email *</label>
-                                    <input type="text" id="email" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-lg-4">
-                                <div class="form-group">
-                                    <label for="website">Website</label>
-                                    <input type="text" id="website" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-lg-4">
-                                <div class="submit-btn">
-                                    <a href="#" class="btn btn-black">Post Comment</a>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div> -->
+                
             </div>
         </section>
     </div>
     <!--=================================
   Brands Slider
 ===================================== -->
-    <section class="section-margin">
-        <h2 class="sr-only">Brand Slider</h2>
-        <div class="container">
-            <div class="brand-slider sb-slick-slider border-top border-bottom" data-slick-setting='{
-                                            "autoplay": true,
-                                            "autoplaySpeed": 8000,
-                                            "slidesToShow": 6
-                                            }' data-slick-responsive='[
-                {"breakpoint":992, "settings": {"slidesToShow": 4} },
-                {"breakpoint":768, "settings": {"slidesToShow": 3} },
-                {"breakpoint":575, "settings": {"slidesToShow": 3} },
-                {"breakpoint":480, "settings": {"slidesToShow": 2} },
-                {"breakpoint":320, "settings": {"slidesToShow": 1} }
-            ]'>
-                <div class="single-slide">
-                    <img src="image/others/brand-1.jpg" alt="">
-                </div>
-                <div class="single-slide">
-                    <img src="image/others/brand-2.jpg" alt="">
-                </div>
-                <div class="single-slide">
-                    <img src="image/others/brand-3.jpg" alt="">
-                </div>
-                <div class="single-slide">
-                    <img src="image/others/brand-4.jpg" alt="">
-                </div>
-                <div class="single-slide">
-                    <img src="image/others/brand-5.jpg" alt="">
-                </div>
-                <div class="single-slide">
-                    <img src="image/others/brand-6.jpg" alt="">
-                </div>
-                <div class="single-slide">
-                    <img src="image/others/brand-1.jpg" alt="">
-                </div>
-                <div class="single-slide">
-                    <img src="image/others/brand-2.jpg" alt="">
-                </div>
-            </div>
-        </div>
-    </section>
+
     <!--=================================
     Footer Area
 ===================================== -->
